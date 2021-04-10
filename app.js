00f3904ffb6835e7ed93e156698c6bce00430489
matthewiharding <matthew.i.harding@gmail.com>
2021-04-10 10:45:28 -0700

    boilerplate

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const http = require('http');
+
+const hostname = '127.0.0.1'
+const port = 3000;
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+})
