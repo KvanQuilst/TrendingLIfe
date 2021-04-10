f2a7941b140065c9679e01d03dacdd49e0382e4b
devanyk2 <devanyk2@wwu.edu>
2021-04-10 12:33:04 -0700

    merging in

--- js/pixel_location.js
+++ js/pixel_location.js
@@ -2,7 +2,7 @@
 module.exports = {
     // 
     pixel_coordinate: function (x,y,c, dim) {
-        var coordinate = (((x*dim) + y) * 4);
+        var coordinate = (((y*dim) + x) * 4);
         return coordinate +  c;
     }
 };
--- test/test_pixel_location.js
+++ test/test_pixel_location.js
@@ -9,4 +9,14 @@ describe('pixel_location', function(){
             assert.strictEqual(0, pixel_coordinate(0,0,0, 100));
         })
     })
+    describe('5th pixel tests', function(){
+        it('Should return just the channel value', function(){
+            assert.strictEqual(20, pixel_coordinate(5,0,0, 100));
+        })
+    })
+    describe('5-3 pixel tests', function(){
+        it('Should return just the channel value', function(){
+            assert.strictEqual(1220, pixel_coordinate(5,3,0, 100));
+        })
+    })
 });
